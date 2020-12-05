01fb3890fd89a1b3c45fb8666676b14cb80cf63c
Paul Jimenez <pj@place.org>
2020-12-04 23:01:03 -0500

    up to day 3 all at once

--- src/day02.zig
+++ src/day02.zig
@@ -3,6 +3,49 @@ const Allocator = std.mem.Allocator;
 const assert = std.debug.assert;
 const print = std.debug.print;
 
-pub fn main() !void {
+const input = @embedFile("../in02.txt");
 
+pub fn main() !void {
+    var inputit = std.mem.tokenize(input, "\n");
+
+    var valids: usize = 0;
+    while (inputit.next()) |line| {
+        var lineit = std.mem.tokenize(line, " -:");
+        var minlets = lineit.next() orelse @panic("no minlet");
+        var minleti = try std.fmt.parseInt(usize, minlets, 10);
+        var maxlets = lineit.next() orelse @panic("no maxlet");
+        var maxleti = try std.fmt.parseInt(usize, maxlets, 10);
+        var let = lineit.next() orelse @panic("no letter");
+        var password = lineit.next() orelse @panic("no password");
+
+        var count: usize = 0;
+        for (password) |c| {
+            if (let[0] == c) count += 1;
+        }
+        if (count < minleti) continue;
+        if (count > maxleti) continue;
+        valids += 1;
+    }
+    print("Found {} valid passwords according to the old rules\n", .{valids});
+
+    inputit = std.mem.tokenize(input, "\n");
+    var newvalids: usize = 0;
+    while (inputit.next()) |line| {
+        var lineit = std.mem.tokenize(line, " -:");
+        var minlets = lineit.next() orelse @panic("no minlet");
+        var minleti = try std.fmt.parseInt(usize, minlets, 10);
+        var maxlets = lineit.next() orelse @panic("no maxlet");
+        var maxleti = try std.fmt.parseInt(usize, maxlets, 10);
+        var let = lineit.next() orelse @panic("no letter");
+        var password = lineit.next() orelse @panic("no password");
+
+        var count: usize = 0;
+        if (password.len >= minleti and password[minleti - 1] == let[0]) count += 1;
+        if (password.len >= maxleti and password[maxleti - 1] == let[0]) count += 1;
+        print("found {} occurances of {c} between positions {} and {} in {}\n", .{ count, let[0], minleti, maxleti, password });
+        if (count != 1) continue;
+        newvalids += 1;
+        print("that makes {}\n", .{newvalids});
+    }
+    print("Found {} valid passwords according to the new rules\n", .{newvalids});
 }
--- src/day03.zig
+++ src/day03.zig
@@ -3,6 +3,62 @@ const Allocator = std.mem.Allocator;
 const assert = std.debug.assert;
 const print = std.debug.print;
 
-pub fn main() !void {
+const maptxt = @embedFile("../in03.txt");
 
+fn width(comptime input: []const u8) usize {
+    var i: usize = 0;
+    while (i < input.len) : (i += 1) {
+        if (input[i] == '\n') return i;
+    }
+    return i;
+}
+
+const mapwidth = width(maptxt);
+const linelength = mapwidth + 1;
+
+fn length(comptime input: []const u8) usize {
+    return input.len / (width(input) + 1);
+}
+
+const maplength = length(maptxt);
+
+const map = @ptrCast(*const [maplength][linelength]u8, &map);
+
+fn cast_atpos(x: usize, y: usize) u8 {
+    print("finding char at ({}, {})\n", .{ x, y });
+    return map[y][x % linelength];
+}
+
+fn math_atpos(x: usize, y: usize) u8 {
+    var i = (y * linelength) + (x % mapwidth);
+    if (i >= map.len) {
+        print("map is only {}\n", .{map.len});
+        assert(i < map.len);
+    }
+    //print("turned ({}, {}) into {} -> {c}\n", .{ x, y, i, maptxt[i] });
+    return maptxt[i];
+}
+
+var atpos = cast_atpos;
+
+fn count_trees(dx: usize, dy: usize) usize {
+    var trees: usize = 0;
+    var x: usize = 0;
+    var y: usize = 0;
+    while (y < maplength) : (y += dy) {
+        if (atpos(x, y) == '#') trees += 1;
+        x += dx;
+    }
+    print("Found {} trees at slope ({}, {})\n", .{ trees, dx, dy });
+    return trees;
+}
+
+pub fn main() !void {
+    print("Map is {}x{}\n", .{ mapwidth, maplength });
+    var answer = count_trees(1, 1) *
+        count_trees(3, 1) *
+        count_trees(5, 1) *
+        count_trees(7, 1) *
+        count_trees(1, 2);
+    print("for an answer of {}\n", .{answer});
 }
